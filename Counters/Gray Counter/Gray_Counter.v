11160a0a6ae766b47999dfcef983aeec6a8c2ee3
Jheel Makhijani <127103262+JheelMak@users.noreply.github.com>
2023-10-19 10:42:50 +0530

    Add files via upload

---
+++ Counters/Gray Counter/Gray_Counter.v
@@ -0,0 +1,18 @@
+module gray_counter (out,count,clk,rst);
+  input clk, rst; 
+  output reg [3:0] out;
+  output reg [3:0] count;
+  always @(posedge clk) 
+  begin
+  if (rst) 
+  begin
+    count = 0; 
+	 out = 0;
+	end
+  else  
+  begin
+	count = count + 1; 
+   out = {count[3], (count[3] ^ count[2]), (count[2] ^ count[1]),(count[1] ^ count[0])};
+   end
+  end
+endmodule
---
+++ Counters/Gray Counter/gray_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+module gray_tb;
+
+	reg clk;
+	reg rst;
+
+	wire [3:0] out;
+
+	gray_counter uut (.out(out), .count(count), .clk(clk), .rst(rst));
+	
+	initial begin
+	forever #5 clk=~clk;
+	end
+
+	initial begin
+   clk = 0; rst=0;
+			$monitor($time," clk=%b, out=%b, count=%b",clk,out,count);
+	#10;
+    rst = 1'b1;
+   #50;
+    rst = 1'b0;
+  #500 $finish;
+
+
+	end
+      
+endmodule
+
